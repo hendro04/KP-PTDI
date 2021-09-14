45c3e35c972f05a3d963681096fd8cfcf6ddb8b6
rhenaldy12 <rhenaldy12@gmail.com>
2021-09-14 20:17:10 +0700

    Create formlogin_admin.php
    tampilan login admin

---
+++ formlogin_admin.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Login</title>
+    </head>
+<body>
+    <h1>Login Admin</h1>
+    <form action="">
+        <label for="">Username : </label>
+        <input type="text"><br>
+        <label for="">Password</label>
+        <input type="password">
+    </form>
+</body>
+</html>
