c2ee26775d8df47d01c4c73f5880c2a01ada0222
rhenaldy12 <rhenaldy12@gmail.com>
2021-09-14 20:57:19 +0700

    Update formlogin_admin.php
    tambahan button login

--- formlogin_admin.php
+++ formlogin_admin.php
@@ -5,10 +5,20 @@
 <body>
     <h1>Login Admin</h1>
     <form action="">
-        <label for="">Username : </label>
-        <input type="text"><br>
-        <label for="">Password</label>
-        <input type="password">
+        <table border="0" cellpadding="0">
+            <tr>
+                <td><label for="">Username : </label></td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td><label for="">Password :</label></td>
+                <td><input type="password"></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" name="login"></td>
+            </tr>
+        </table>
     </form>
 </body>
 </html>
