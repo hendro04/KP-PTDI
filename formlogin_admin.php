e3d3e3f6f18cc9e1f030b496c02850fe16918247
rhenaldy12 <rhenaldy12@gmail.com>
2021-09-15 20:46:29 +0700

    update

--- formlogin_admin.php
+++ formlogin_admin.php
@@ -1,22 +1,22 @@
 <html>
     <head>
         <title>Login</title>
+        <link rel=”stylesheet” type=”text/css” href=”login.css”
     </head>
 <body>
-    <h1>Login Admin</h1>
-    <form action="">
+    <h3>Login Admin</h1>
+    <form class="login">
         <table border="0" cellpadding="0">
             <tr>
                 <!--<td><label for="">Username</label></td>-->
-                <td><input type="text" placeholder="Username"></td>
+                <td><input type="text" placeholder="Username Admin"></td>
             </tr>
             <tr>
                 <!--<td><label for="">Password</label></td>-->
                 <td><input type="password" placeholder="Password"></td>
             </tr>
             <tr>
-                <td></td>
-                <td><input type="submit" name="login"></td>
+                <td><input type="submit" name="login" value="login"></td>
             </tr>
         </table>
     </form>
